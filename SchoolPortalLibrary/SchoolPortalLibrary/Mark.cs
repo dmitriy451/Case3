e81e51ce800dde7924d00b67a61576b92d72223d
dmitriy451 <76558463+dmitriy451@users.noreply.github.com>
2022-03-15 19:26:45 +0300

    Library
    Library Added

---
+++ SchoolPortalLibrary/SchoolPortalLibrary/Mark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPortalLibrary
+{
+    internal class Mark
+    {
+        public Mark(DateTime date, string estimation)
+        {
+            Date = date;
+            Estimation = estimation;
+        }
+        public DateTime Date { get; set; }
+
+        public string Estimation { get; set; }
+    }
+}
